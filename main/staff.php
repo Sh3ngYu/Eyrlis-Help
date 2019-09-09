7ce32e7e085707346c4daf60ba5a628ff6d4d4c8
Sh3ngYu <52979466+Sh3ngYu@users.noreply.github.com>
2019-09-09 16:17:33 +0200

    Add files via upload

---
+++ main/staff.php
@@ -0,0 +1,140 @@
+<!-- SG Update -->
+<br><br>
+<style>
+  .table-borderless tr.bg-dark > th {
+    border-radius: 0.25rem 0 0 0.25rem;
+}
+.table-borderless tr.bg-dark > th, .table-borderless tr.bg-dark > td {
+    padding: 1rem;
+    position: relative;
+}
+
+
+</style>
+
+	<section class="flush-with-above">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+
+				<h1 class="h2 mb-2" style="color:white;">Staff Presentation</h1>
+				<span>If you need to talk to a GM or staff member, you can reach us through the game or Discord, but please be wary of staff impersonators that may try to scam you. Staff members will never ask for your password, and will never accept money from anyone.</span>
+                <br><br>
+				
+			<table class="table table-hover align-items-center table-borderless" style="background-color: #454c54;border-radius:12px;">
+                <thead>
+                  <tr>
+                    <th scope="col" style="color:white;">Username</th>
+                    <th scope="col" style="color:white;">Role</th>
+                    <th scope="col" style="color:white;">Languages</th>
+                    <th scope="col" style="color:white;"></th>
+                  </tr>
+                </thead>
+				
+                <tbody>
+					<tr class="bg-dark" style="background-color: #2d3238 !important;">
+						<th scope="row">
+						<div class="media align-items-center">
+							<img class="avatar" style="width: 7.5rem; height: 7.5rem;" src="<?php echo $this->themepath('img/ragnarok/ChlasAvat.png');?>">
+							<div class="media-body">
+								  <span class="h6 mb-0">Chlas
+									<span class="badge badge-primary">Admin</span>
+								  </span>
+								  <span class="text-muted">Co-Founder & Administrator</span>
+							</div>
+						</div>
+						</th>
+                    <td>Emulator, Scripts, Developement, Mapping</td>
+                    <td>FR / EN</td>
+                  </tr>
+              
+					<tr class="bg-dark" style="background-color: #2d3238 !important;">
+						<th scope="row">
+						<div class="media align-items-center">
+							<img class="avatar" style="width: 7.5rem; height: 7.5rem;" src="<?php echo $this->themepath('img/ragnarok/ill_032.png');?>">
+							<div class="media-body">
+								<span class="h6 mb-0">Pochic
+									<span class="badge badge-info">SysAdmin</span>
+								</span>
+							  <span class="text-muted">Network Administrator</span>
+							</div>
+						</div>
+						</th>
+                    <td>SysAdmin & Security manager</td>
+                    <td>FR / EN</td>
+					</tr>
+        
+					<tr class="bg-dark" style="background-color: #373d44 !important;">
+						<th scope="row">
+						<div class="media align-items-center">
+							<img class="avatar" style="width: 7.5rem; height: 7.5rem;" src="<?php echo $this->themepath('img/ragnarok/bdUcspZ.jpg');?>">
+							<div class="media-body">
+								<span class="h6 mb-0">Nyliar
+									<span class="badge badge-warning">Developer</span>
+								</span>
+							  <span class="text-muted">Developer</span>
+							</div>
+						</div>
+                    </th>
+                    <td>Scripts, Plugins developement</td>
+                    <td>FR / EN</td>
+                  </tr>
+				</tbody>
+			</table>
+			
+			<h1 class="h2 mb-2" style="color:white;">Guards</h1>
+			<span>Guards have no real power. It's a reward given at kind and helpfull player. They keep the realm of Eyrlis safe, and can answer at all your questions.</span>
+			<br><br>
+			
+			<table class="table table-hover align-items-center table-borderless" style="background-color: #454c54;border-radius:12px;">
+                <thead>
+                  <tr>
+                    <th scope="col" style="color:white;">Username</th>
+                    <th scope="col" style="color:white;">Role</th>
+                    <th scope="col" style="color:white;">Languages</th>
+                    <th scope="col" style="color:white;"></th>
+                  </tr>
+                </thead>
+				
+                <tbody>			
+					<tr class="bg-dark" style="background-color: #2d3238 !important;">
+						<th scope="row">
+						<div class="media align-items-center">
+							<img class="avatar" style="width: 7.5rem; height: 7.5rem;" src="<?php echo $this->themepath('img/ragnarok/Sebas.png');?>">
+							<div class="media-body">
+							<span class="h6 mb-0">Sebas
+								<span class="badge badge-success">Support</span>
+							</span>
+                          <span class="text-muted">Guard & Support</span>
+                        </div>
+                      </div>
+                    </th>
+                    <td>Supervising & Support</td>
+                    <td>FR / EN</td>
+                  </tr>
+
+                    <tr class="bg-dark" style="background-color: #373d44 !important;">
+                    <th scope="row">
+                      <div class="media align-items-center">
+                        <img class="avatar" style="width: 7.5rem; height: 7.5rem;" src="<?php echo $this->themepath('img/ragnarok/kinzy.jpg');?>">
+                        <div class="media-body">
+                          <span class="h6 mb-0">Kinzy
+                            <span class="badge badge-success">Support</span>
+                          </span>
+                          <span class="text-muted">Guard & Support</span>
+                        </div>
+                      </div>
+                    </th>
+                    <td>Supervising & Support</td>
+                    <td>FR / EN</td>
+                  </tr>
+                </tbody>
+			</table>
+			  
+            </div>
+            <!--end of col-->
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
